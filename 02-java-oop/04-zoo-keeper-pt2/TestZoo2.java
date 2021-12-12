90b1f956a7fa7f80c1bedcf51303a9f7a4535cbc
Tranzorro <shiekadam@gmail.com>
2021-12-12 17:10:08 -0500

    fixed zoo2
    errors fixed.

--- 02-java-oop/04-zoo-keeper-pt2/TestZoo2.java
+++ 02-java-oop/04-zoo-keeper-pt2/TestZoo2.java
@@ -1,18 +1,21 @@
-import Creature;
-import Bat;
+// import Creature;
+// import Bat;
 public class TestZoo2 {
-    Bat bat = new Bat();
-    String name = bat.name("Dracula");
-    int fullness = bat.fullness(0);
-    boolean full = bat.full(false);
-    int Energy = bat.Energy(300);
 
-    // below is complaining for the need of an identifier?
-    bat.AttackTown();
-    bat.AttackTown();
-    bat.AttackTown();
-    bat.eatHumans();
-    bat.eatHumans();
-    bat.Fly();
-    bat.Fly();
+    public static void main(String[] args){
+
+        Bat bat = new Bat();
+        bat.name = "Dracula";
+        bat.fullness = 0;
+        bat.full = false;
+        bat.Energy = 300;
+    
+        bat.AttackTown();
+        bat.AttackTown();
+        bat.AttackTown();
+        bat.eatHumans();
+        bat.eatHumans();
+        bat.Fly();
+        bat.Fly();
+    }
 }
